150ccb8f9e2a740de8de0f1d5ea7025349a36fe7
Vineet Kumar <kvineet569@gmail.com>
2019-10-09 12:34:02 -0600

    take remote of PC

--- Powershell/TakeRemote.ps1
+++ Powershell/TakeRemote.ps1
@@ -1,6 +1,6 @@
 ﻿#This script take remote desktop of PC whose hostname is passed.
 #To run this script open powershell and navigate to script location folderand type following way::
-#.\TakeRemote.ps1 "Hostname" "Password" "UserName"
+#.\TakeRemote.ps1 "Hostname" "Password" "UserName."
 param([String]$HostName,[String]$Password,[string]$User)
 cmdkey /generic:TERMSRV/$HostName /user:$User /pass:$Password
 mstsc /v:$HostName
